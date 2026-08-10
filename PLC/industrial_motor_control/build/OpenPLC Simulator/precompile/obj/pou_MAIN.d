1e80e6a1bed723d8f0528127d358905e0dc51341
8068joshua <8068joshua@gmail.com>
2026-08-10 23:21:28 +0900

    Implementing Basic motor Control and Temperature Interlock

---
+++ PLC/industrial_motor_control/build/OpenPLC Simulator/precompile/obj/pou_MAIN.d
@@ -0,0 +1,34 @@
+D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\precompile\obj\pou_MAIN.o: \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\precompile\sources\pou_MAIN.cpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/generated.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_types.hpp \
+ C:\Users\문영제\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.10\include/cstdint \
+ C:\Users\문영제\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.10\include/cstddef \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_var.hpp \
+ C:\Users\문영제\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.10\include/type_traits \
+ C:\Users\문영제\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.10\include/utility \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_global.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_array.hpp \
+ C:\Users\문영제\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.10\include/array \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_fault.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_located.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_std_lib.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_traits.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_retain.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_ptr.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_string.hpp \
+ C:\Users\문영제\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.10\include/cstdio \
+ C:\Users\문영제\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.10\include/cstring \
+ C:\Users\문영제\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.10\include/cstdlib \
+ C:\Users\문영제\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.10\include/algorithm \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_wstring.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_time.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_date.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_dt.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_tod.hpp \
+ C:\Users\문영제\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.10\include/cmath \
+ C:\Users\문영제\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.10\include/chrono \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_enum.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_memory.hpp \
+ D:\OpenPLC\industrial_motor_control\build\OpenPLC\ Simulator\src/iec_pointer.hpp \
+ C:\Users\문영제\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.10\include/string
---
+++ PLC/industrial_motor_control/build/OpenPLC Simulator/src/defines.h
@@ -0,0 +1,26 @@
+
+
+//Program MD5
+#define PROGRAM_MD5 "e00314fe63c42aaea44499e319b1b6a9"
+
+//Comms Configuration
+#define SIMULATOR_MODE
+#define MBSERIAL_IFACE Serial
+#define MBSERIAL_BAUD 115200
+#define MBSERIAL_SLAVE 1
+#define MBSERIAL
+#define MODBUS_ENABLED
+
+
+//IO Config
+#define PINMASK_DIN 
+#define PINMASK_AIN 
+#define PINMASK_DOUT 
+#define PINMASK_AOUT 
+#define NUM_DISCRETE_INPUT 0
+#define NUM_ANALOG_INPUT 0
+#define NUM_DISCRETE_OUTPUT 0
+#define NUM_ANALOG_OUTPUT 0
+
+
+//Arduino libraries
